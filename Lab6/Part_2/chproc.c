6024513477db9063d59a0dc39e8568274f862e62
pmjonesg <pmjonesg@gmail.com>
2014-03-26 17:48:40 -0400

    modified:   chproc.c

--- Lab6/Part_2/chproc.c
+++ Lab6/Part_2/chproc.c
@@ -5,34 +5,38 @@
 #include <time.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/times.h>
+
+void childInt(int signum)
+{
+	fprintf(stdout, "SIGCHILD Interrupt \n");
+}
 
 void h(int signum)
 {
-  fprintf(stdout, "CHLD Interrupt");
+  struct tms start, end;
+  struct timeval time;
+	long clockTicksPerSecond = sysconf(_SC_CLK_TCK);
+
+	times(&start);
+  system("last | sort -r | wc > /dev/null");
+	signal(SIGCHLD, childInt);
+	times(&end);
+
+	fprintf(stdout, "Time used by user child: %8.4lf seconds.\n", (end.tms_cutime - start.tms_cutime) / (double) clockTicksPerSecond);
+	fprintf(stdout, "Time used by system child: %8.5lf seconds.\n", (end.tms_cstime - start.tms_cstime) / (double) clockTicksPerSecond);
+  
 }
 
 int main(int argc, char **argv)
 {
- // struct tms cur_time;
-  struct timeval time;
-
   void h(int);
   int i;
   
   fprintf(stdout, "Process id: %d\nParent process id: %d\n\n", getpid(), getppid());
 
-  system("last | sort -r | wc > /dev/null");
-  
-  signal(SIGCHLD, h);
-
-  for(i=0;i<10;i++)
-  { 
-    fprintf(stdout, "%d", i);
-    sleep(5);
-  }
-
-
-  
+  signal(SIGINT, h);
+  signal(SIGINT, h);
  
   return 0;
 }
