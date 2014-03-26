fb6a94c02a5c6a61e543ca51cf33cdb0f43fb461
pmjonesg <pmjonesg@gmail.com>
2014-03-26 19:45:14 -0400

    modified:   DBhandler.c 	modified:   findPerson.c

--- Lab6/Part_2/DBhandler.c
+++ Lab6/Part_2/DBhandler.c
@@ -5,22 +5,44 @@
 #include "person.h"
 #include "findPerson.c"
 
+static int offset = 60;
 static FILE *myDB;
 
 void firstInterrupt(int signum)
 {
- findPerson();
+ findPerson(0);
+}
 
+void secondInterrupt(int signum)
+{
+  findPerson(offset);
+	offset += 60;
+}
+
+void thirdInterrupt(int signum)
+{
+  static int previous = 60;
+	findPerson(offset - previous);
+	if(offset != 60)
+	  previous += 60;
+	else ;
 }
 
 int main(int argc, char **argv)
 {
 	void firstInterrupt(int);
+	void secondInterrupt(int);
   FILE *myDB = fopen("myDB", "r");
 
   signal(SIGINT, firstInterrupt);
-
 	kill(getpid(), SIGINT);
+
+	signal(SIGINT, secondInterrupt);
+	raise(SIGINT);
+
+	signal(SIGINT, thirdInterrupt);
+	raise(SIGINT);
+
   fclose(myDB);
 
 	for(;;) sleep(1);
--- Lab6/Part_2/findPerson.c
+++ Lab6/Part_2/findPerson.c
@@ -14,25 +14,16 @@ ourse: CENG411
 #include <errno.h>
 #include "person.h"
 
-						void findPerson()
+						void findPerson(long cursor)
 {
 				  struct PERSON person;
-					  char response[512];
 
 						  FILE *myDB=fopen("myDB","r");
-							  fprintf(stdout,"The size of an PERSON is: %d\n\n",sizeof person);
-								  while(true)
-													  {
-																		    fprintf(stdout,"Enter a record number. (-1 exits): ");
-																				    fflush(stdout);
-																						    fgets(response,10,stdin);
-																								    if(atoi(response)<0) break;
-																										    fseek(myDB, atoi(response)*sizeof person, SEEK_SET);
+																										    fseek(myDB, cursor, SEEK_SET);
 																												    fprintf(stdout,"\nPosition in file before reading it: %ld\n", ftell(myDB));
 																														    fread(&person,sizeof person,1,myDB);
 																																    displayPerson(person);
 																																		    fprintf(stdout,"Position in file after reading it: %ld\n\n", ftell(myDB));
 																																				    
-																																				   }
 									  fclose(myDB);  
 											  }
