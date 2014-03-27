c2dc6f46ee3eedf7899ceb8493265b7b5aa6f465
pmjonesg <pmjonesg@gmail.com>
2014-03-26 21:27:19 -0400

    modified:   sigusr1.c 	modified:   ../Part_1/findPerson.c

--- Lab6/Part_2/sigusr1.c
+++ Lab6/Part_2/sigusr1.c
@@ -1,17 +1,25 @@
+/*
+ * File:    sigusr1.c
+ * Course:  CENG411
+ * Author:  Peter Jones
+ * Date:    Wednesday 26, 2014
+ *
+ */
+
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
 #include <time.h>
 
+
 void h(int signum)
 {
-
   char * cmd = "ps | wc -l";
   char buffer[100];
-	FILE *log, *process;
+  FILE *log, *process;
   time_t when=0L;
   
-  // Output interrupt to screen
+  /*  Output interrupt to screen */
   if((process = popen(cmd, "r")) != NULL)
   {
     while(fgets(buffer, 100, process) != NULL)
@@ -19,26 +27,31 @@ void h(int signum)
     pclose(process);
   }
 
-  //Write date and # of processes to log.txt
+  /* Write date and # of processes to log.txt */
   log = fopen("log.txt", "a+");
   fprintf(log, "Time: %sProcess Id: %s\n\n", ctime(&when), buffer);
   fclose(log);
 }
 
-int main()
+/* Start main program */
+int main(int argc, char **argv)
 {
+  /* Declare handler */
   void handler(int);
   int i;
 
-  //Print # of process
+  /* Print # of process */
   fprintf(stdout, "The current process id is: %d " "parent is: %d\n", getpid(), getppid());
   
+  /* Catch signal SIGUSR1 */
   signal(SIGUSR1, h);
 
-  //Loop to handle interrupts
-  for(i = 0; i < 10; i++) { fprintf(stdout, "Id: %d\n", i);
-                           sleep(6);
-                         }
+  /* Loop to handle interrupts */
+  for(i = 0; i < 10; i++)
+  { 
+    fprintf(stdout, "Id: %d\n", i);
+    sleep(6);
+  }
 
-	return 0;
+  return 0;
 }
