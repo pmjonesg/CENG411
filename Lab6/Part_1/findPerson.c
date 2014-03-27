64cdf15c4848f02b243ccf1807c8941dba930237
pmjonesg <pmjonesg@gmail.com>
2014-03-26 20:49:31 -0400

    modified:   findPerson.c

--- Lab6/Part_1/findPerson.c
+++ Lab6/Part_1/findPerson.c
@@ -1,11 +1,12 @@
 /*
-   File: findPerson.c
-	 Course: CENG411
-   Author: Leon King,J230,x4200
-   Date: Wednesday Feb 03, 2010   21:32 PM
- 
-   Program will prompt you for a record number
-   Using random access it will retrieve the specified record
+*  File: findPerson.c
+* Course: CENG411
+*  Author: Peter Jones
+*  Date: Wednesday 26, March 2014
+*
+*  Program will create a random person then read the record straigt away.
+* It demonstrates that when opening a file in a+ mode you can write as well
+* as read (writing at the end of the file and reading at the beginning)
 */
 
 
@@ -16,6 +17,7 @@
 #include "person.h"
 #include <string.h>
 
+/* Declaration of names for random generation */
 char names[][10]={ "George","Ann","Renata","Betty","Sam","Vince",
                  "Stephane","Ringo","Helen","Elmer","Steve",
                  "Aaron","Kim","George","Paul","John","Jean Luc","Colm" };
@@ -27,6 +29,7 @@ char role[][25]={"Captain","Ensign","Red Shirt","Red Shirt","Red Shirt",
                  "Crew","Transporter Chief","Morale Officer","Crew",
                  "Crew","Navigator","Engineer"};
 
+/* This function generates a random person from the arrays listed above */
 void makePerson(struct PERSON *p)
 {
    strcpy(p->firstName,names[rand()%sizeof(names)/sizeof(names[0])]);
@@ -36,36 +39,36 @@ void makePerson(struct PERSON *p)
    p->sex = (rand()%2) ? 'M' : 'F';
    }
 
+/* Start main program */
 int main(int argc, char * argv[],char * envp[])
 {
   struct PERSON person;
   char response[512];
 
+	/* Open file which already contains data */
   FILE *myDB=fopen("myDB","a+");
+
+	/* Create a random person (will always create the same person as the first time) */
   makePerson(&person);
   fwrite(&person, sizeof(person),1, myDB);
-	fseek(myDB, 0L, SEEK_END);
-	int sz = ftell(myDB);
-	fprintf(stdout, "Size of myDB is: %d\n", sz);
-	person.recordNo = (int) (sz/60);
+
+  /* Get position of cursor */
+	long sz = ftell(myDB);
+	//fprintf(stdout, "Size of myDB is: %ld\n", sz);
+	person.recordNo = (int) (sz/ sizeof(person));
+	fprintf(stdout, "Person created\n");
   displayPerson(person);
-  sprintf(response, "%d", sz/60);
 
-  fprintf(stdout,"The size of an PERSON is: %lu\n\n",sizeof person);
-  while(true)
-  {
-    fprintf(stdout,"Enter a record number. (-1 exits): ");
-    fflush(stdout);
-    fgets(response,10,stdin);
-		if(atoi(response)<0) break;
-    fseek(myDB, atoi(response)*sizeof person, SEEK_SET);
-    fprintf(stdout,"\nPosition in file before reading it: %ld\n", ftell(myDB));
-    fread(&person,sizeof person,1,myDB);
-    displayPerson(person);
-    fprintf(stdout,"Position in file after reading it: %ld\n\n", ftell(myDB));
+	/* Print firt record read (should be the first one */
+	fprintf(stdout, "\nRecord read");
+  fseek(myDB, sz, SEEK_SET);
+  fprintf(stdout,"\nPosition in file before reading it: %ld\n", ftell(myDB));
+  fread(&person,sizeof person,1,myDB);
+  displayPerson(person);
+  fprintf(stdout,"Position in file after reading it: %ld\n\n", ftell(myDB));
     
-   }
+	/* Close file and end program */
   fclose(myDB);  
-  return 0;
-  }
 
+  return 0;
+}
