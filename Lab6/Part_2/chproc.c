23d849863c27545a0782805ea88b6bc6307fd801
pmjonesg <pmjonesg@gmail.com>
2014-03-27 00:16:15 -0400

    modified:   chproc.c

--- Lab6/Part_2/chproc.c
+++ Lab6/Part_2/chproc.c
@@ -1,3 +1,12 @@
+/*
+ * File:    chproc.c
+ * Course:  CENG411
+ * Author:  Peter Jones
+ * Date:    Wednesday 26 March, 2014
+ * Purpose: This program creates a child process when SIGINT is sent and measures the time that
+ *          the child process used
+ */
+
 #include <stdio.h>
 #include <signal.h>
 #include <stdlib.h>
@@ -7,36 +16,44 @@
 #include <sys/types.h>
 #include <sys/times.h>
 
+/* This handler signals that a SIGCHLD interrupt has been catched */
 void childInt(int signum)
 {
 	fprintf(stdout, "SIGCHILD Interrupt \n");
 }
 
+/* This handler creates the child and calculates the time */
 void h(int signum)
 {
-  struct tms start, end;
-  struct timeval time;
+	struct tms start, end;
+	struct timeval time;
 	long clockTicksPerSecond = sysconf(_SC_CLK_TCK);
 
+	/* Start measuring time for process just before it happens */
 	times(&start);
-  system("last | sort -r | wc > /dev/null");
+	system("last | sort -r | wc > /dev/null");
+	/* Send SIGCHLD signal AFTER the process has been created */
 	signal(SIGCHLD, childInt);
 	times(&end);
 
+	/* Display the time the process took */
 	fprintf(stdout, "Time used by user child: %8.4lf seconds.\n", (end.tms_cutime - start.tms_cutime) / (double) clockTicksPerSecond);
 	fprintf(stdout, "Time used by system child: %8.5lf seconds.\n", (end.tms_cstime - start.tms_cstime) / (double) clockTicksPerSecond);
   
 }
 
+/* Start of main program */
 int main(int argc, char **argv)
 {
-  void h(int);
-  int i;
+	/* Declare handler */
+	void h(int);
+	int i;
   
-  fprintf(stdout, "Process id: %d\nParent process id: %d\n\n", getpid(), getppid());
+	fprintf(stdout, "Process id: %d\nParent process id: %d\n\n", getpid(), getppid());
 
-  signal(SIGINT, h);
-  signal(SIGINT, h);
+	/* Send signal to create children */
+	signal(SIGINT, h);
+	signal(SIGINT, h);
  
-  return 0;
+	return 0;
 }
