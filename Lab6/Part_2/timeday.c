bcf26c5ea904e45794e4cd4dcbaf2b0ee3f58295
pmjonesg <pmjonesg@gmail.com>
2014-03-27 00:06:21 -0400

    modified:   timeday.c

--- Lab6/Part_2/timeday.c
+++ Lab6/Part_2/timeday.c
@@ -1,3 +1,12 @@
+/*
+ *  File:    timeday.c
+ *  Course:  CENG411
+ *  Author:  Peter Jones
+ *  Date:    Wednesday 26 March, 2014
+ *  Purpose: This program displays the current time of day then exits with a code of 7
+ *
+ */
+
 #include <stdio.h>
 #include <time.h>
 #include <signal.h>
@@ -6,33 +15,38 @@
 #include <sys/time.h>
 #include <stdlib.h>
 
-void h(int signum)
+void h()
 {
-  struct timeval currtime;
-  struct tm* current;
-  char time_string[40];
+	struct timeval currtime;
+	struct tm* current;
+	char time_string[40];
 
-  gettimeofday(&currtime, NULL);
-  current = localtime(&currtime.tv_sec);
+	/* Get time of day */
+	gettimeofday(&currtime, NULL);
+	current = localtime(&currtime.tv_sec);
 
-  strftime(time_string, sizeof(time_string), "%H:%M:%S", current);
-  fprintf(stdout, "Current time of day: %s\n", time_string);
-  exit(7);
+	/* Format time */
+	strftime(time_string, sizeof(time_string), "%H:%M:%S", current);
+	fprintf(stdout, "Current time of day: %s\n", time_string);
+
 }
 
+/* Start of main program */
 int main(int argc, char **argv)
 {
-  void h(int);
-  int i;
+	int i;
+	fprintf(stdout, "Current process: %d\n Parent process id: %d\n", getpid(), getppid());
 
-  fprintf(stdout, "Current process: %d\n Parent process id: %d\n", getpid(), getppid());
+	/* Declare handler */
+	atexit(h);
 
-  signal(SIGINT, h);
+	for(i = 0; i < 5; i++)
+	{
+		fprintf(stdout, "Loop %d\n", i);
+		sleep(1);
+	}
 
-  for(i=0;i<10;i++) { 
-  fprintf(stdout, "Interrupt: %d\n", i);
-  sleep(7);
-  }
+/* Exit to call interrupt */
+	exit(7);
 
-  return 0;
 }
